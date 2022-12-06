07994df5de5856ddf728b32ef21b072e65cf8f1d
admin <mmsaj000@hotmail.com>
2022-12-06 15:54:17 +0300

    from pc-- fix problem

--- lib/ui/setting_page.dart
+++ lib/ui/setting_page.dart
@@ -1,20 +1,14 @@
-import 'package:alkhudhrah_app/constants/api_const.dart';
 import 'package:alkhudhrah_app/constants/colors.dart';
 import 'package:alkhudhrah_app/designs/appbar_design.dart';
-import 'package:alkhudhrah_app/designs/bottom_nav_bar.dart';
-import 'package:alkhudhrah_app/designs/drawer_design.dart';
-import 'package:alkhudhrah_app/helper/shared_pref_helper.dart';
+
 import 'package:alkhudhrah_app/locale/locale_keys.g.dart';
-import 'package:alkhudhrah_app/network/models/driver_user.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 import '../constants/cont.dart';
 import '../helper/route_helper.dart';
-import '../helper/snack_message.dart';
-import 'companies/company_order_history.dart';
+
 import 'contact_us.dart';
 import 'home.dart';
 import 'language_setting.dart';
